56b9cdea336a4c8aae68bbec4d9a45f58488c959
Nat Tuck <nat@ferrus.net>
2023-09-14 08:43:45 -0400

    latest style test

--- src/main/resources/archetype-resources/test.pl
+++ src/main/resources/archetype-resources/test.pl
@@ -3,132 +3,69 @@ use 5.26.0;
 use warnings FATAL => 'all';
 use feature qw(signatures);
 no warnings "experimental::signatures";
+use lib './.lib';
 
-use Test::Simple tests => 3;
-use XML::Parser;
+use Test::Simple tests => 18;
 use Data::Dumper;
 
-my $par = XML::Parser->new(Style => 'Objects', Pkg => 'X');
-my $tree = $par->parsefile('target/surefire-reports/TEST-goat.AppShould.xml');
+our %EXPECT_TESTS = (
+    Prob1Should => 9,
+    Prob2Should => 1,
+    Prob3Should => 6,
+    Prob4Should => 1,
+);
+our $PKG = "lab03";
 
-for my $node (@$tree) {
-    $node->visit();
-}
+use JUnit;
 
-package X::testsuite;
+my @reports = JUnit::list_reports();
 
-sub visit($self) {
-    my $tests = $self->{tests};
-    my $fails = $self->{failures};
-    my $errors = $self->{errors};
-
-    say "# Tests: $tests, Fails: $fails, Errors: $errors";
-
-    for my $kid (@{$self->{Kids}}) {
-	$kid->visit();
+my %seen = map { $_ => 1 } @reports;
+for my $xx (sort keys %EXPECT_TESTS) {
+    my $name = "$PKG.$xx";
+    unless (exists($seen{$name})) {
+        say "# Test report missing: $name";
+        say "# Giving up";
+        exit(0);
     }
-}
 
-package X::properties;
+    ok(1, "Test output exists for $name");
 
-sub visit($self) {
-    for my $kid (@{$self->{Kids}}) {
-	$kid->visit();
-    }
-}
+    my $report = JUnit::read_report($name);
 
-package X::property;
+    my $jvers = $report->java_version();
+    say "# Java VM: $jvers";
 
-sub visit($self) {
-    my $name = $self->{name};
-    my $value = $self->{value};
-
-    if ($name eq 'java.runtime.version') {
-	say "# java.runtime.version: $value";
-    }
-}
-
-package X::testcase;
-
-use Test::Simple;
-
-sub visit($self) {
-    my $name = $self->{name};
-    my $fails = $self->count_fails();
-    ok($fails == 0, $name);
-    if ($fails > 0) {
-	say $self->get_comment();
-    }
-}
-
-sub count_fails($self) {
-    my $fails = 0;
-    for my $kid (@{$self->{Kids}}) {
-	$fails += $kid->count_fails();
-    }
-    return $fails;
-}
-
-sub get_comment($self) {
-    my $text = "";
-    for my $kid (@{$self->{Kids}}) {
-	$text .= $kid->get_comment();
-    }
-    return $text;
-}
-
-package X::failure;
-
-use Data::Dumper;
-
-sub count_fails($self) {
-    return 1;
-}
-
-sub get_comment($self) {
-    my $text = "";
-    for my $kid (@{$self->{Kids}}) {
-	$text .= $kid->get_comment();
-    }
-    return $text;
-}
-
-package X::error;
-
-use Data::Dumper;
-
-sub count_fails($self) {
-    return 1;
-}
-
-sub get_comment($self) {
-    my $text = "";
-    for my $kid (@{$self->{Kids}}) {
-	$text .= $kid->get_comment();
-    }
-    return $text;
-}
-
-package X::Characters;
-
-sub visit($self) {
-    # do nothing
-}
-
-sub count_fails($self) {
-    # Text isn't a fail
-    return 0;
-}
-
-sub get_comment($self) {
-    my $text = $self->{Text};
-    if ($text =~ /^\s*$/) {
-	return "";
+    my $summary = $report->summary();
+    my $expect = $EXPECT_TESTS{$xx};
+    my $ran = $summary->{tests};
+    my $good = ($ran == $expect);
+    if ($good) {
+        ok(1, "Ran $expect tests in $name");
     }
     else {
-	$text = "\n$text\n";
-	$text =~ s/^/# /mg;
-	$text =~ s/\n+$//;
-	return $text;
+        ok(0, "In $name, ran $ran, expected $expect tests");
+    }
+    ok($good && $summary->{fails} == 0, "Expected tests ran and no failures for $name");
+    ok($good && $summary->{errors} == 0, "Expected tests ran and no errors for $name");
+}
+
+use CheckStyle;
+
+my @errors = CheckStyle->list_errors();
+my $count = scalar @errors;
+
+ok($count < 10, "Less than 10 style errors");
+ok($count == 0, "No style errors");
+
+if ($count > 0) {
+    say "#";
+    say "# You had some style errors, here they are:";
+    for my $err (@errors) {
+        my $file = $err->{file};
+        $file =~ s/^.*\///;
+        my $line = $err->{line};
+        my $text = $err->{text};
+        say "#   $file:$line => $text";
     }
 }
